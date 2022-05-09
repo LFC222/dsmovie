f1dd2673622d47e2f7faa543c5da06bd70d72b1f
Lucas FC <lucasfernandes_222@hotmail.com>
2022-05-09 18:19:03 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+import './styles.css';
+
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a href="https://github.com/LFC222">
+            <div className="dsmovie-contact-conatainer">
+              <GithubIcon />
+              <p className="dsmovie-contact-link">/devsuperior </p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
