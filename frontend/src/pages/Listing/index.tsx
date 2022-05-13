8ee701d2de33f6fba20ed9769d8951ed24637693
Lucas FC <lucasfernandes_222@hotmail.com>
2022-05-13 12:49:34 -0300

    use state e use effect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,30 @@
 import axios from "axios";
 import MovieCard from "components/MoveCard";
 import Pagination from "components/Pagination";
+import { useEffect } from "react";
+import { useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/request";
 
 function Listing() {
 
-  axios.get(`${BASE_URL}/movies?size=12&page=0`)
+  const[pageNumber, setPageNumber] = useState(0);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=1`)
     .then(response => {
-      console.log(response.data);
+      const data = response.data as MoviePage
+      console.log(data);
+      setPageNumber(data.number);
     });
+  },[]);
+
+  
 
 
   return (
     <>
+    <p>{pageNumber}</p>
       <Pagination />
       <div className="container">
         <div className="row">
